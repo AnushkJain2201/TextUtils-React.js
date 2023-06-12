951ee70fd12848dbca54026af16e1d2c43f9f9c9
Anushk Jain <anushk2201@gmail.com>
2023-06-13 05:02:20 +0530

    Pakka Final ab

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -26,7 +26,7 @@ export default function Navbar(props) {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav me-auto mb-2 mb-lg-0">
             <li className="nav-item">
-              <Link to='/' className="nav-link active" aria-current="page">
+              <Link to='/' className="nav-link" aria-current="page">
                 Home
               </Link>
             </li>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -52,7 +52,7 @@ export default function TextForm(props) {
       </div>
       <div className="container my-3" style={{color: props.mode === 'light'?'black':'white'}}>
         <h2>Your Text Summary</h2>
-        <p>{text.split(" ").filter((element) => {return element.length !== 0}).length} words, {text.length} characters</p>
+        <p>{text.split(/\s+/).filter((element) => {return element.length !== 0}).length} words, {text.length} characters</p>
         <p>{0.008 * text.split(" ").filter((element) => {return element.length !== 0}).length} Minutes read</p>
         <h2>Preview</h2>
         <p>{text.length>0?text:"Nothing To Preview"}</p>
